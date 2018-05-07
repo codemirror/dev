b4fc522cfb3d004aff4cd2e869ce7fb7d0d9dfee
Adrian Heine <mail@adrianheine.de>
2018-05-07 08:10:16 +0200

    Fix syntax in rollup.demo.config.js

--- rollup.demo.config.js
+++ rollup.demo.config.js
@@ -4,7 +4,7 @@ export default {
   input: "./demo/demo.ts",
   output: {
     format: "umd",
-    file: "./demo/demo_built.js"
+    file: "./demo/demo_built.js",
     sourcemap: true
   },
   plugins: [typescript()]
