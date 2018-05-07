642faafe414774755f6ec25689ec4d1065245ddd
Adrian Heine <mail@adrianheine.de>
2018-05-07 09:34:33 +0200

    Initial history implementation
    This is heavily based on prosemirror-history, especially the tests.

---
+++ history/src/history.ts
@@ -0,0 +1,270 @@
+import {Text} from "../../doc/src/text"
+import {Change, EditorState, StateFieldSpec, Transaction} from "../../state/src/state"
+import {Mappable, Mapping, StepMap} from "prosemirror-transform"
+
+// Used to schedule history compression
+const max_empty_items = 500
+
+const key = "history"
+const closeHistoryKey = "history_close"
+
+function getStepMap(change: Change) {
+  return new StepMap([change.from, change.to - change.from, change.text.length])
+}
+
+function mapChange(map: Mappable, change: Change): Change {
+  const from = map.map(change.from, 1)
+  return new Change(from, Math.max(from, map.map(change.to, -1)), change.text)
+}
+
+class ChangesEntry {
+  constructor(readonly changes: ReadonlyArray<Change>,
+              readonly inverted: ReadonlyArray<Change>) {}
+}
+
+class MappingEntry {
+  constructor(readonly map: Mapping) {};
+
+  static fromChanges(changes: ReadonlyArray<Change>): MappingEntry {
+    return new MappingEntry(new Mapping(changes.map(getStepMap)))
+  }
+}
+
+type HistoryEntry = ChangesEntry | MappingEntry
+
+const enum PopTarget {
+  Done,
+  Undone
+}
+
+class Branch {
+  private constructor(public readonly entries: ReadonlyArray<HistoryEntry>,
+                      public readonly eventCount: number) {}
+
+  addChanges(changes: ReadonlyArray<Change>, docs: ReadonlyArray<Text>, tryMerge: boolean): Branch {
+    const inverted = changes.map((c, i) => c.invert(docs[i]))
+    const lastItem = this.entries[this.entries.length - 1]
+    if (lastItem instanceof ChangesEntry &&
+        tryMerge &&
+        isAdjacent(lastItem.changes[lastItem.changes.length - 1], changes[0])) {
+      const newEntries = this.entries.slice()
+      newEntries[newEntries.length - 1] = new ChangesEntry(lastItem.changes.concat(changes), lastItem.inverted.concat(inverted))
+      return new Branch(newEntries, this.eventCount)
+    } else {
+      return this.pushEntry(new ChangesEntry(changes, inverted))
+    }
+  }
+
+  pushEntry(entry: HistoryEntry): Branch {
+    return new Branch(this.entries.concat(entry),
+                      this.eventCount + (entry instanceof ChangesEntry ? 1 : 0))
+  }
+
+  popChanges(): {changesEntry: ChangesEntry, newBranch: Branch, map: Mapping} | null {
+    let idx
+    for (idx = this.entries.length - 1; this.entries[idx] instanceof MappingEntry; --idx) {}
+    if (!this.entries[idx]) return null
+
+    const map = new Mapping()
+    for (let i = idx + 1; i < this.entries.length; ++i) {
+      const entry = this.entries[i] as MappingEntry
+      map.appendMapping(entry.map)
+    }
+
+    let init: ReadonlyArray<HistoryEntry> = this.entries.slice(0, idx)
+    // if there are no changes before map entries, we don't keep the map entries either
+    if (init.length) {
+      init = init.concat(new MappingEntry(map))
+    }
+    return {changesEntry: this.entries[idx] as ChangesEntry, newBranch: new Branch(init, this.eventCount - 1), map}
+  }
+
+  rebase(changes: ReadonlyArray<Change>, docs: ReadonlyArray<Text>, mapping: Mapping, rebasedCount: number): Branch {
+    if (!this.eventCount) return this
+
+    const rebasedItems = [], start = Math.max(0, this.entries.length - rebasedCount)
+    let newUntil = changes.length
+    let eventCount = this.eventCount
+
+    let iRebased = rebasedCount
+    for (let i = start; i < this.entries.length; ++i) {
+      const entry = this.entries[i]
+      const pos = mapping.getMirror(--iRebased)
+      if (pos == null) {
+        if (entry instanceof ChangesEntry) --eventCount
+        continue
+      }
+      newUntil = Math.min(newUntil, pos)
+      if (entry instanceof ChangesEntry) {
+        rebasedItems.push(new ChangesEntry([changes[pos]], [changes[pos].invert(docs[pos])]))
+      } else {
+        rebasedItems.push(entry)
+      }
+    }
+
+    const newMap = new Mapping
+    for (let i = rebasedCount; i < newUntil; i++) newMap.appendMap(mapping.maps[i])
+    const entries = this.entries.slice(0, start).concat([new MappingEntry(newMap)]).concat(rebasedItems)
+    let branch = new Branch(entries, eventCount)
+    if (entries.length - eventCount > max_empty_items)
+      branch = branch.compress(this.entries.length - rebasedItems.length)
+    return branch
+  }
+
+  // FIXME: This only compresses consecutive `MappingEntry`s instead of mapping
+  // `ChangeEntry`s and discarding `MappingEntry`s.
+  compress(upto: number = this.entries.length): Branch {
+    const newEntries = []
+    let eventCount = 0
+    for (let map = null, i = 0; i < upto; ++i) {
+      const entry = this.entries[i]
+      if (entry instanceof MappingEntry) {
+        if (!map) map = new Mapping()
+        map.appendMapping(entry.map)
+      } else {
+        if (map) {
+          newEntries.push(new MappingEntry(map))
+          map = null
+        }
+        newEntries.push(entry)
+        ++eventCount
+      }
+    }
+    if (map) {
+      newEntries.push(new MappingEntry(map))
+      map = null
+    }
+    return new Branch(newEntries.concat(this.entries.slice(upto)), eventCount)
+  }
+
+  static readonly empty: Branch = new Branch([], 0)
+}
+
+class HistoryState {
+  constructor(public readonly done: Branch,
+              public readonly undone: Branch,
+              private readonly prevTime: number | null = null) {}
+
+  addChanges(changes: ReadonlyArray<Change>, docs: ReadonlyArray<Text>, time: number, newGroupDelay: number): HistoryState {
+    if (changes.length == 0) return this
+    return new HistoryState(this.done.addChanges(changes, docs, this.prevTime !== null && time - this.prevTime < newGroupDelay),
+                            this.undone, time)
+  }
+
+  rebase(changes: ReadonlyArray<Change>, docs: ReadonlyArray<Text>, mapping: Mapping, rebasedCount: number): HistoryState {
+    return new HistoryState(this.done.rebase(changes, docs, mapping, rebasedCount),
+                            this.undone.rebase(changes, docs, mapping, rebasedCount),
+                            this.prevTime)
+  }
+
+  addMapping(entry: MappingEntry): HistoryState {
+    if (this.done.entries.length == 0) return this
+    return new HistoryState(this.done.pushEntry(entry), this.undone)
+  }
+
+  canPop(done: PopTarget): boolean {
+    return (done == PopTarget.Done ? this.done : this.undone).entries.length > 0
+  }
+
+  pop(done: PopTarget): {changes: ReadonlyArray<Change>, state: HistoryState, map: Mapping} {
+    const popResult = (done == PopTarget.Done ? this.done : this.undone).popChanges()
+    if (!popResult) {
+      if (this.canPop(done)) throw new Error("LogicError: canPop returns true but pop fails")
+      throw new Error("Shouldn't call pop if canPop returns false")
+    }
+    const {newBranch, changesEntry, map} = popResult
+    let changes = [], inverted = []
+    for (let i = 0; i < changesEntry.changes.length; ++i) {
+      const change = changesEntry.changes[i]
+      if (!map.mapResult(change.from, 1).deleted && !map.mapResult(change.from + change.text.length, -1).deleted) {
+        changes.unshift(mapChange(map, change))
+        inverted.unshift(mapChange(map, changesEntry.inverted[i]))
+      }
+    }
+    let otherSide = (done == PopTarget.Done ? this.undone : this.done)
+    if (changes.length > 0) {
+      otherSide = otherSide.pushEntry(new ChangesEntry(inverted, changes))
+    }
+    return {
+      changes: changesEntry.inverted,
+      state: new HistoryState(done == PopTarget.Done ? newBranch : otherSide,
+                              done == PopTarget.Done ? otherSide : newBranch),
+      map
+    }
+  }
+}
+
+function isAdjacent(prev: Change | null, cur: Change): boolean {
+  if (!prev) return true
+  return cur.from <= prev.mapPos(prev.to) && cur.to >= prev.mapPos(prev.from, -1)
+}
+
+export function history(options: {newGroupDelay?: number} = {}): {stateField: StateFieldSpec<HistoryState>} {
+  const {newGroupDelay = 500} = options
+  return {
+    stateField: {
+      key,
+
+      init(): HistoryState {
+        return new HistoryState(Branch.empty, Branch.empty)
+      },
+
+      apply(tr: Transaction, state: HistoryState): HistoryState {
+        const fromMeta = tr.getMeta(key)
+        if (fromMeta) return fromMeta
+        if (tr.getMeta(closeHistoryKey)) state = new HistoryState(state.done, state.undone, null)
+        let rebased
+        if (tr.getMeta("addToHistory") !== false) {
+          return state.addChanges(tr.changes, [tr.startState.doc].concat(tr.docs), tr.getMeta("time"), newGroupDelay)
+        } else if (rebased = tr.getMeta("rebased")) {
+          return state.rebase(tr.changes, [tr.startState.doc].concat(tr.docs), tr.getMeta("mapping"), rebased)
+        } else {
+          return state.addMapping(MappingEntry.fromChanges(tr.changes))
+        }
+      }
+    }
+  }
+}
+
+function historyCmd(target: PopTarget, state: EditorState, dispatch?: (tr: Transaction) => void | null): boolean {
+  const historyState: HistoryState | null = state.getField(key)
+  if (!historyState || !historyState.canPop(target)) return false
+  if (dispatch) {
+    const {changes, state: newState, map} = historyState.pop(target)
+    let tr = state.transaction.setMeta(key, newState)
+    let revChanges = changes.slice().reverse()
+    for (let change of revChanges) {
+      change = mapChange(map, change)
+      tr = tr.change(change)
+    }
+    dispatch(tr)
+  }
+  return true
+}
+
+export function undo(state: EditorState, dispatch?: (tr: Transaction) => void | null): boolean {
+  return historyCmd(PopTarget.Done, state, dispatch)
+}
+
+export function redo(state: EditorState, dispatch?: (tr: Transaction) => void | null): boolean {
+  return historyCmd(PopTarget.Undone, state, dispatch)
+}
+
+// Set a flag on the given transaction that will prevent further steps
+// from being appended to an existing history event (so that they
+// require a separate undo command to undo).
+export function closeHistory(tr: Transaction): Transaction {
+  return tr.setMeta(closeHistoryKey, true)
+}
+
+// The amount of undoable events available in a given state.
+export function undoDepth(state: EditorState): number {
+  let hist = state.getField(key)
+  return hist ? hist.done.eventCount : 0
+}
+
+// The amount of redoable events available in a given state.
+export function redoDepth(state: EditorState): number {
+  let hist = state.getField(key)
+  return hist ? hist.undone.eventCount : 0
+}
---
+++ history/test/test-history.ts
@@ -0,0 +1,364 @@
+const ist = require("ist")
+
+import {Mapping, StepMap} from "prosemirror-transform"
+
+import {Change, EditorState, Range, Selection, Transaction} from "../../state/src/state"
+import {closeHistory, history, redo, redoDepth, undo, undoDepth} from "../src/history"
+
+const mkState = (config?) => EditorState.create({fields: [history(config).stateField]})
+
+const type = (state, text, at = state.doc.length) => state.transaction.replace(at, at, text).apply()
+const timedType = (state, text, atTime) => Transaction.start(state, atTime).replace(state.doc.length, state.doc.length, text).apply()
+const receive = (state, text, from, to = from) => state.transaction.replace(from, to, text).setMeta("addToHistory", false).apply()
+const command = (state, cmd) => {
+  ist(cmd(state, tr => state = tr.apply()), true)
+  return state
+}
+
+const compress = state => {
+  state.fields.history.done = state.fields.history.done.compress()
+  return state
+}
+
+describe("history", () => {
+  it("historyStateField can be used in an EditorState", () => {
+    mkState()
+  })
+
+  it("historyStateField.init returns a HistoryState object", () => {
+    ist(history().stateField.init())
+  })
+
+  it("allows to undo a change", () => {
+    let state = mkState()
+    state = type(state, "newtext")
+    state = command(state, undo)
+    ist(state.doc.text, "")
+  })
+
+  it("allows to undo nearby changes in one change", () => {
+    let state = mkState()
+    state = type(state, "new")
+    state = type(state, "text")
+    state = command(state, undo)
+    ist(state.doc.text, "")
+  })
+
+  it("allows to redo a change", () => {
+    let state = mkState()
+    state = type(state, "newtext")
+    state = command(state, undo)
+    state = command(state, redo)
+    ist(state.doc.text, "newtext")
+  })
+
+  it("allows to redo nearby changes in one change", () => {
+    let state = mkState()
+    state = type(state, "new")
+    state = type(state, "text")
+    state = command(state, undo)
+    state = command(state, redo)
+    ist(state.doc.text, "newtext")
+  })
+
+  it("tracks multiple levels of history", () => {
+    let state = mkState()
+    state = type(state, "new")
+    state = type(state, "text")
+    state = type(state, "some", 0)
+    ist(state.doc.text, "somenewtext")
+    state = command(state, undo)
+    ist(state.doc.text, "newtext")
+    state = command(state, undo)
+    ist(state.doc.text, "")
+    state = command(state, redo)
+    ist(state.doc.text, "newtext")
+    state = command(state, redo)
+    ist(state.doc.text, "somenewtext")
+    state = command(state, undo)
+    ist(state.doc.text, "newtext")
+  })
+
+  it("starts a new event when newGroupDelay elapses", () => {
+    let state = mkState({newGroupDelay: 1000})
+    state = timedType(state, "a", 1000)
+    state = timedType(state, "b", 1600)
+    ist(undoDepth(state), 1)
+    state = timedType(state, "c", 2700)
+    ist(undoDepth(state), 2)
+    state = command(state, undo)
+    state = timedType(state, "d", 2800)
+    ist(undoDepth(state), 2)
+  })
+
+  it("allows changes that aren't part of the history", () => {
+    let state = mkState()
+    state = type(state, "hello")
+    state = receive(state, "oops", 0)
+    state = receive(state, "!", 9)
+    state = command(state, undo)
+    ist(state.doc.text, "oops!")
+  })
+
+  it("doesn't get confused by an undo not adding any redo item", () => {
+    let state = mkState()
+    state = type(state, "foo")
+    state = receive(state, "bar", 0, 3)
+    state = command(state, undo)
+    ist(redo(state), false)
+  })
+
+  function unsyncedComplex(state, doCompress) {
+    state = type(state, "hello")
+    state = closeHistory(state.transaction).apply()
+    state = type(state, "!")
+    state = receive(state, "....", 0)
+    state = type(state, "\n\n", 2)
+    ist(state.doc.text, "..\n\n..hello!")
+    state = receive(state, "\n\n", 1)
+    if (doCompress) compress(state)
+    state = command(state, undo)
+    state = command(state, undo)
+    ist(state.doc.text, ".\n\n...hello")
+    state = command(state, undo)
+    ist(state.doc.text, ".\n\n...")
+  }
+
+  it("can handle complex editing sequences", () => {
+    unsyncedComplex(mkState(), false)
+  })
+
+  it("can handle complex editing sequences with compression", () => {
+    unsyncedComplex(mkState(), true)
+  })
+
+  it("supports overlapping edits", () => {
+    let state = mkState()
+    state = type(state, "hello")
+    state = closeHistory(state.transaction).apply()
+    state = state.transaction.replace(0, 5, "").apply()
+    ist(state.doc.text, "")
+    state = command(state, undo)
+    ist(state.doc.text, "hello")
+    state = command(state, undo)
+    ist(state.doc.text, "")
+  })
+
+  it("supports overlapping edits that aren't collapsed", () => {
+    let state = mkState()
+    state = receive(state, "h", 0)
+    state = type(state, "ello")
+    state = closeHistory(state.transaction).apply()
+    state = state.transaction.replace(0, 5, "").apply()
+    ist(state.doc.text, "")
+    state = command(state, undo)
+    ist(state.doc.text, "hello")
+    state = command(state, undo)
+    ist(state.doc.text, "h")
+  })
+
+  it("supports overlapping unsynced deletes", () => {
+    let state = mkState()
+    state = type(state, "hi")
+    state = closeHistory(state.transaction).apply()
+    state = type(state, "hello")
+    state = state.transaction.replace(0, 7, "").setMeta("addToHistory", false).apply()
+    ist(state.doc.text, "")
+    state = command(state, undo)
+    ist(state.doc.text, "")
+  })
+
+  it("can go back and forth through history multiple times", () => {
+    let state = mkState()
+    state = type(state, "one")
+    state = type(state, " two")
+    state = closeHistory(state.transaction).apply()
+    state = type(state, " three")
+    state = type(state, "zero ", 0)
+    state = closeHistory(state.transaction).apply()
+    state = type(state, "\n\n", 0)
+    state = type(state, "top", 0)
+    for (let i = 0; i < 6; i++) {
+      let re = i % 2
+      for (let j = 0; j < 4; j++) state = command(state, re ? redo : undo)
+      ist(state.doc.text, re ? "top\n\nzero one two three" : "")
+    }
+  })
+
+  it("supports non-tracked changes next to tracked changes", () => {
+    let state = mkState()
+    state = type(state, "o")
+    state = type(state, "\n\n", 0)
+    state = receive(state, "zzz", 3)
+    state = command(state, undo)
+    ist(state.doc.text, "zzz")
+  })
+
+  it("can go back and forth through history when preserving items", () => {
+    let state = mkState()
+    state = type(state, "one")
+    state = type(state, " two")
+    state = closeHistory(state.transaction).apply()
+    state = receive(state, "xxx", state.doc.length)
+    state = type(state, " three")
+    state = type(state, "zero ", 0)
+    state = closeHistory(state.transaction).apply()
+    state = type(state, "\n\n", 0)
+    state = type(state, "top", 0)
+    state = receive(state, "yyy", 0)
+    for (let i = 0; i < 3; i++) {
+      if (i == 2) compress(state)
+      for (let j = 0; j < 4; j++) state = command(state, undo)
+      ist(state.doc.text, "yyyxxx")
+      for (let j = 0; j < 4; j++) state = command(state, redo)
+      ist(state.doc.text, "yyytop\n\nzero one twoxxx three")
+    }
+  })
+
+  it("restores selection on undo", () => {
+    let state = mkState()
+    state = type(state, "hi")
+    state = closeHistory(state.transaction).apply()
+    state = state.transaction.setSelection(new Selection([new Range(0, 2)])).apply()
+    const selection = state.selection
+    state = state.transaction.replaceSelection("hello").apply()
+    const selection2 = state.selection
+    state = command(state, undo)
+    ist(state.selection.eq(selection))
+    state = command(state, redo)
+    ist(state.selection.eq(selection2))
+  })
+
+  it("rebases selection on undo", () => {
+    let state = mkState()
+    state = type(state, "hi")
+    state = closeHistory(state.transaction).apply()
+    state = state.transaction.setSelection(new Selection([new Range(0, 2)])).apply()
+    state = type(state, "hello", 0)
+    state = receive(state, "---", 0)
+    state = command(state, undo)
+    ist(state.selection.ranges[0].head, 5)
+  })
+
+  it.skip("handles change overwriting in item-preserving mode", () => {
+    let state = mkState(undefined, {preserveItems: true})
+    state = type(state, "a")
+    state = type(state, "b")
+    state = closeHistory(state.transaction).apply()
+    state = state.transaction.setSelection(new Selection([new Range(0, 2)])).apply()
+    state = type(state, "c")
+    state = command(state, undo)
+    state = command(state, undo)
+    ist(state.doc.text, "")
+  })
+
+  it("supports querying for the undo and redo depth", () => {
+    let state = mkState()
+    state = type(state, "a")
+    ist(undoDepth(state), 1)
+    ist(redoDepth(state), 0)
+    state = receive(state, "b", 0)
+    ist(undoDepth(state), 1)
+    ist(redoDepth(state), 0)
+    state = command(state, undo)
+    ist(undoDepth(state), 0)
+    ist(redoDepth(state), 1)
+    state = command(state, redo)
+    ist(undoDepth(state), 1)
+    ist(redoDepth(state), 0)
+  })
+
+  it("all functions gracefully handle EditorStates without history", () => {
+    let state = EditorState.create()
+    ist(undoDepth(state), 0)
+    ist(redoDepth(state), 0)
+    ist(undo(state), false)
+    ist(redo(state), false)
+  })
+
+  it.skip("truncates history", () => {
+    let state = mkState(null, {depth: 2})
+    for (let i = 1; i < 40; ++i) {
+      state = type(state, "a")
+      state = closeHistory(state.transaction).apply()
+      ist(undoDepth(state), (i - 2) % 21 + 2)
+    }
+  })
+
+  it("supports transactions with multiple changes", () => {
+    let state = mkState()
+    state = state.transaction.replace(0, 0, "a").replace(1, 1, "b").apply()
+    state = type(state, "c", 0)
+    ist(state.doc.text, "cab")
+    state = command(state, undo)
+    ist(state.doc.text, "ab")
+    state = command(state, undo)
+    ist(state.doc.text, "")
+    state = command(state, redo)
+    ist(state.doc.text, "ab")
+    state = command(state, redo)
+    ist(state.doc.text, "cab")
+    state = command(state, undo)
+    ist(state.doc.text, "ab")
+  })
+
+  it("supports rebasing", () => {
+    // This test simulates a collab editing session where the local editor
+    // receives a change (`right`) that's on top of the parent change (`base`) of
+    // the last local change (`left`).
+
+    // Shared base change
+    let state = mkState()
+    state = type(state, "base")
+    state = closeHistory(state.transaction).apply()
+    const baseDoc = state.doc
+
+    // Local unconfirmed change
+    //
+    //        - left
+    //       /
+    // base -
+    //       \
+    //        - right
+    let rightChange = new Change(4, 4, " right")
+    state = state.transaction.change(rightChange).apply()
+    ist(state.doc.text, "base right")
+    ist(undoDepth(state), 2)
+    let leftChange = new Change(0, 0, "left ")
+
+    // Receive remote change and rebase local unconfirmed change
+    //
+    // base --> left --> right'
+    let tr = state.transaction
+    tr = tr.change(rightChange.invert(baseDoc))
+    tr = tr.change(leftChange)
+    tr = tr.change(new Change(leftChange.mapPos(rightChange.from, 1), leftChange.mapPos(rightChange.to, -1), rightChange.text))
+
+    function getStepMap(change: Change) {
+      return new StepMap([change.from, change.to - change.from, change.text.length])
+    }
+    const mapping = new Mapping()
+    mapping.appendMap(getStepMap(rightChange.invert(baseDoc)))
+    mapping.appendMap(getStepMap(leftChange))
+    mapping.appendMap(getStepMap(new Change(leftChange.mapPos(rightChange.from, 1), leftChange.mapPos(rightChange.to, -1), rightChange.text)), 0)
+    tr = tr.setMeta("mapping", mapping)
+
+    tr = tr.setMeta("addToHistory", false)
+    tr = tr.setMeta("rebased", 1)
+    state = tr.apply()
+    ist(state.doc.text, "left base right")
+    ist(undoDepth(state), 2)
+
+    // Undo local unconfirmed change
+    //
+    // base --> left
+    state = command(state, undo)
+    ist(state.doc.text, "left base")
+
+    // Redo local unconfirmed change
+    //
+    // base --> left --> right'
+    state = command(state, redo)
+    ist(state.doc.text, "left base right")
+  })
+})
