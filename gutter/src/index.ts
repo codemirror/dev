b58bf18b094300f7137afec5ff96fcccb12e4da6
Marijn Haverbeke <marijn@haverbeke.nl>
2018-07-23 13:52:21 +0200

    Set role=presentation on the gutter
    It'll be useless for screen readers to start reading line numbers (and
    even if there's useful info in there, it'll be almost impossible to
    correlate it with a specific line in the text part of the editor).

--- gutter/src/index.ts
+++ gutter/src/index.ts
@@ -36,6 +36,7 @@ class GutterView {
   constructor(view: EditorView, config: GutterConfig) {
     this.dom = document.createElement("div")
     this.dom.className = "CodeMirror-gutter"
+    this.dom.setAttribute("role", "presentation")
     this.dom.style.cssText = `flex-basis: 0; left: 0`
     if (config.fixed !== false) {
       // FIXME IE11 fallback, which doesn't support position: sticky,
