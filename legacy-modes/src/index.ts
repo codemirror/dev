e4cebddc1188f5abdc91db0790a9acf938a1e581
Adrian Heine <mail@adrianheine.de>
2018-09-16 09:07:47 +0200

    Cleanup legacy-modes

--- legacy-modes/src/index.ts
+++ legacy-modes/src/index.ts
@@ -1,38 +1,107 @@
+// This is a temporary, simplified version of the mode plugin, which
+// handles huge documents better. Doesn't always highlight accurately.
+
 import {EditorView} from "../../view/src"
-import {Text} from "../../doc/src/text"
 import {Range} from "../../rangeset/src/rangeset"
-import {EditorState, Plugin, StateField} from "../../state/src"
-import {DecoratedRange, Decoration} from "../../view/src/decoration"
+import {EditorState, Plugin, StateField, Transaction} from "../../state/src"
+import {Decoration} from "../../view/src/decoration"
 
 import {StringStreamCursor} from "./stringstreamcursor"
-import {DecorationCache} from "./decorationcache"
 import {copyState, readToken, Mode} from "./util"
 
-function getDecorations<S>(mode: Mode<S>, doc: Text, from: number, to: number, state?: S): [ReadonlyArray<DecoratedRange>, ReadonlyArray<Range<S>>] {
-  state = state ? copyState(mode, state) : mode.startState()
-  const decorations = []
-  const states: Range<S>[] = []
-  const cursor = new StringStreamCursor(doc, from)
-  let stream = cursor.next()
-  const pushState = () => states.push(new Range(stream.pos + cursor.offset, stream.pos + cursor.offset, copyState(mode, state)))
-  for (let line = 0; cursor.offset < to; stream = cursor.next(), ++line) {
-    while (!stream.eol()) {
-      const style = readToken(mode, stream, state)
-      if (style) decorations.push(Decoration.range(stream.start + cursor.offset, stream.pos + cursor.offset,
-                                                   {class: 'cm-' + style.replace(/ /g, ' cm-')}))
-      stream.start = stream.pos
-      if ((stream.pos + 1) % 4096 == 0) pushState()
+class CachedState<S> {
+  constructor(public state: S, public pos: number) {}
+  copy(mode: Mode<S>) { return new CachedState(copyState(mode, this.state), this.pos) }
+}
+
+const MAX_SCAN_DIST = 20000
+
+class StateCache<S> {
+  constructor(private states: CachedState<S>[], private frontier: number) {}
+
+  getDecorations(editorState: EditorState, from: number, to: number, mode: Mode<S>): Range<Decoration>[] {
+    let state = this.getState(editorState, from, mode)
+    let cursor = new StringStreamCursor(editorState.doc, from, editorState.tabSize)
+    let states: CachedState<S>[] = [], decorations: Range<Decoration>[] = [], stream = cursor.next()
+    for (let i = 0, pos = from; pos < to;) {
+      if (stream.eol()) {
+        pos++
+        stream = cursor.next()
+        if (++i % 5 == 0) states.push(new CachedState(copyState(mode, state), pos))
+      } else {
+        let style = readToken(mode, stream, state), len = stream.pos - stream.start
+        if (style)
+          decorations.push(Decoration.range(pos, pos + len, {class: 'cm-' + style.replace(/ /g, ' cm-')}))
+        stream.start = stream.pos
+        pos += len
+      }
     }
-    if ((line + 1) % 5 == 0) pushState()
+    this.storeStates(from, to, states)
+    return decorations
   }
-  pushState()
-  return [decorations, states]
+
+  storeStates(from: number, to: number, states: CachedState<S>[]) {
+    let start = this.findIndex(from), end = this.findIndex(to)
+    this.states.splice(start, end - start, ...states)
+    if (from < this.frontier) this.frontier = Math.max(this.frontier, to)
+  }
+
+  // Return the first index for which all cached states after it have
+  // a position >= pos
+  findIndex(pos: number): number {
+    // FIXME could be binary search
+    let i = 0
+    while (i < this.states.length && this.states[i].pos < pos) i++
+    return i
+  }
+
+  stateBefore(pos: number, mode: Mode<S>): {state: S, pos: number} {
+    if (pos > this.frontier && pos - this.frontier < MAX_SCAN_DIST) pos = this.frontier
+    let index = this.findIndex(pos)
+    if (index < this.states.length && this.states[index].pos == pos) index++
+    return index == 0 ? new CachedState(mode.startState(), 0) : this.states[index - 1].copy(mode)
+  }
+
+  getState(editorState: EditorState, pos: number, mode: Mode<any>): any {
+    let {pos: statePos, state} = this.stateBefore(pos, mode)
+    if (statePos < pos - MAX_SCAN_DIST) { statePos = pos; state = mode.startState() }
+    if (statePos < pos) {
+      let cursor = new StringStreamCursor(editorState.doc, statePos, editorState.tabSize)
+      let stream = cursor.next()
+      let start = statePos, i = 0, states: CachedState<S>[] = []
+      while (statePos < pos) {
+        if (stream.eol()) {
+          stream = cursor.next()
+          statePos++
+          if (++i % 50) states.push(new CachedState(copyState(mode, state), statePos))
+        } else {
+          readToken(mode, stream, state)
+          statePos += stream.pos - stream.start
+          stream.start = stream.pos
+        }
+      }
+      this.storeStates(start, pos, states)
+    }
+    return state
+  }
+
+  apply(transaction: Transaction) {
+    if (transaction.changes.length == 0) return this
+    let start = transaction.changes.changes.reduce((m, ch) => Math.min(m, ch.from), 1e9)
+    let states = []
+    for (let cached of this.states) {
+      let mapped = transaction.changes.mapPos(cached.pos, -1, true)
+      if (mapped > 0) states.push(mapped == cached.pos ? cached : new CachedState(cached.state, mapped))
+    }
+    return new StateCache(states, Math.min(start, this.frontier))
+  }
 }
 
 export function legacyMode<S>(mode: Mode<S>) {
-  const field = new StateField<DecorationCache<S>>({
-    init(state: EditorState) { return new DecorationCache((doc, from, to, startState) => getDecorations(mode, doc, from, to, startState), state.doc) },
-    apply(tr, state) { return state.update(tr) }
+  const field = new StateField<StateCache<S>>({
+    init(state: EditorState) { return new StateCache([], 0) },
+    apply(tr, cache) { return cache.apply(tr) },
+    debugName: "mode"
   })
 
   let plugin = new Plugin({
@@ -43,13 +112,13 @@ export function legacyMode<S>(mode: Mode<S>) {
         let vp = v.viewport
         if (force || vp.from < from || vp.to > to) {
           ;({from, to} = vp)
-          decorations = v.state.getField(field)!.getDecorations(from, to)
+          decorations = Decoration.set(v.state.getField(field)!.getDecorations(v.state, from, to, mode))
         }
       }
       return {
         get decorations() { return decorations },
         updateViewport: update,
-        updateState: (v: EditorView) => update(v, true)
+        updateState: (v: EditorView, p: EditorState, trs: Transaction[]) => update(v, trs.some(tr => tr.docChanged))
       }
     }
   })
@@ -58,21 +127,8 @@ export function legacyMode<S>(mode: Mode<S>) {
   // not sure yet what it'd look like
   ;(plugin as any).indentation = function(state: EditorState, pos: number): number {
     if (!mode.indent) return -1
-    let {pos: statePos, state: modeState} = state.getField(field)!.getStateBefore(pos)
-    let cursor = new StringStreamCursor(state.doc, statePos)
-    let stream = cursor.next()
-    modeState = modeState ? copyState(mode, modeState) : mode.startState()
-    while (statePos < pos) {
-      if (stream.eol()) {
-        stream = cursor.next()
-        statePos++
-      } else {
-        readToken(mode, stream, modeState)
-        statePos += stream.pos - stream.start
-        stream.start = stream.pos
-      }        
-    }
-    return mode.indent(modeState, stream.string.slice(stream.pos).match(/^\s*(.*)/)![1])
+    let modeState = state.getField(field)!.getState(state, pos, mode)
+    return mode.indent(modeState, state.doc.slice(pos, state.doc.lineEndAt(pos))[0].match(/^\s*(.*)/)![1])
   }
 
   return plugin
