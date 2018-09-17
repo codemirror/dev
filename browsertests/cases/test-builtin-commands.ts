ec5e1471ab444c29e9ee39221bfe582b2f4963af
Adrian Heine <mail@adrianheine.de>
2018-09-17 16:22:31 +0200

    Add types to browsertests

--- browsertests/cases/test-builtin-commands.ts
+++ browsertests/cases/test-builtin-commands.ts
@@ -1,31 +1,32 @@
-import {Builder, By, Key} from "selenium-webdriver"
+import {Builder, By, Key, WebElement} from "selenium-webdriver"
 const ist = require("ist")
 
+const target = process.env.TARGET!
 const driver = new Builder().forBrowser("chrome").build()
 const browser = process.env.SELENIUM_BROWSER || "chrome"
 const firefox = browser == "firefox"
 const tests = {
   async setCursor(n: number) { return driver.executeScript(`view.dispatch(view.state.transaction.setSelection(view.state.selection.constructor.single(${n})).scrollIntoView())`) },
-  getSelection() { return driver.executeScript("return view.state.selection") },
-  setText(text: string) { return driver.executeScript(`view.dispatch(view.state.transaction.replace(0, view.state.doc.length, ${JSON.stringify(text)}))`) },
-  getText() { return driver.executeScript(`return view.state.doc.toString()`) }
+  async getCursor(): Promise<number> { return driver.executeScript("return view.state.selection.primary.anchor") as Promise<number> },
+  async setText(text: string) { return driver.executeScript(`view.dispatch(view.state.transaction.replace(0, view.state.doc.length, ${JSON.stringify(text)}))`) },
+  async getText(): Promise<string> { return driver.executeScript(`return view.state.doc.toString()`) as Promise<string> }
 }
 
-let cm = null
+let cm: null | WebElement = null
 const getCm = async function() {
   if (cm) return cm
-  await driver.get(process.env.TARGET)
+  await driver.get(target)
   cm = driver.findElement(By.className("CodeMirror-content"))
   await cm.click()
   return cm
 }
 
 let pending = 0
-const forAllPositions = (onlyValid, f) => async function () {
+const forAllPositions = (onlyValid: boolean, f: (cm: WebElement, i: number, nonspacing: boolean) => Promise<void>) => async function () {
   ++pending
   const cm = await getCm()
   await tests.setText("one\nاِثْنَانِ\nthree\n".repeat(100))
-  const run = async function(i) {
+  const run = async function(i: number) {
     const nonspacing = [5, 7, 9, 12].indexOf(i % 20) != -1
     if (nonspacing && onlyValid) return
     await tests.setCursor(i)
@@ -37,14 +38,14 @@ const forAllPositions = (onlyValid, f) => async function () {
   await run(2000)
   --pending
 }
-const forAllValidPositions = f => forAllPositions(true, f)
+const forAllValidPositions = (f: (cm: WebElement, i: number, nonspacing: boolean) => Promise<void>) => forAllPositions(true, f)
 
 const lines = [
   [        0,        1,  2,  3],
   [  [4, 13], [11, 12], 10,  8,  6, [4, 13]],
   [       14,       15, 16, 17, 18,      19]
 ]
-const getAllCoords = pos => {
+const getAllCoords = (pos: number) => {
   const ret = []
   for (let y = 0; y < lines.length; ++y)
     for (let x = 0; x < lines[y].length; ++x) {
@@ -60,13 +61,13 @@ describe("builtin commands", () => {
   })
 
   it("setCursor self-test", forAllPositions(false, async function (cm, i, nonspacing) {
-    const pos = (await tests.getSelection()).primary.anchor
+    const pos = await tests.getCursor()
     ist(pos == i || (nonspacing && pos == i + 1))
   }))
 
   it("left", forAllValidPositions(async function (cm, start) {
     await cm.sendKeys(Key.LEFT)
-    const result = (await tests.getSelection()).primary.anchor
+    const result = await tests.getCursor()
 
     if (start == 0) {
       ist(result, start)
@@ -83,7 +84,7 @@ describe("builtin commands", () => {
 
   it("right", forAllValidPositions(async function (cm, start) {
     await cm.sendKeys(Key.RIGHT)
-    const result = (await tests.getSelection()).primary.anchor
+    const result = await tests.getCursor()
 
     if (start == 2000) return ist(result, start)
     const acceptable = getAllCoords(start % 20).reduce((acceptable, [y, x]) => {
@@ -97,7 +98,7 @@ describe("builtin commands", () => {
 
   it("up", forAllValidPositions(async function (cm, start) {
     await cm.sendKeys(Key.UP)
-    const result = (await tests.getSelection()).primary.anchor
+    const result = await tests.getCursor()
     if (start < 4) {
       ist(result, 0)
       return
@@ -113,7 +114,7 @@ describe("builtin commands", () => {
 
   it("down", forAllValidPositions(async function (cm, start) {
     await cm.sendKeys(Key.DOWN)
-    const result = (await tests.getSelection()).primary.anchor
+    const result = await tests.getCursor()
     if (start >= 20*99 + 14) {
       ist(result, 20*100)
       return
@@ -132,18 +133,18 @@ describe("builtin commands", () => {
   // The synthesized keydown event has code = ""
   // FIXME goLineStartSmart
   // FIXME longer lines
-  it("home", !firefox && forAllValidPositions(async function (cm, start) {
+  it("home", firefox ? undefined : forAllValidPositions(async function (cm, start) {
     await cm.sendKeys(Key.HOME)
-    const pos = (await tests.getSelection()).primary.anchor
+    const pos = await tests.getCursor()
     const i = start % 20
     const line = (i < 4 ? 0 : (i < 14 ? 1 : 2))
     ist({0: 0, 1: 4, 2: 14}[line], pos % 20)
   }))
 
   // FIXME longer lines
-  it("end", !firefox && forAllValidPositions(async function (cm, start) {
+  it("end", firefox ? undefined : forAllValidPositions(async function (cm, start) {
     await cm.sendKeys(Key.END)
-    const pos = (await tests.getSelection()).primary.anchor
+    const pos = await tests.getCursor()
     if (start == 2000) {
       ist(pos, start)
       return
@@ -153,9 +154,9 @@ describe("builtin commands", () => {
     ist({0: 3, 1: 13, 2: 19}[line], pos % 20)
   }))
 
-  it("page up", !firefox && forAllValidPositions(async function (cm, start) {
+  it("page up", firefox ? undefined : forAllValidPositions(async function (cm, start) {
     await cm.sendKeys(Key.PAGE_UP)
-    const pos = (await tests.getSelection()).primary.anchor
+    const pos = await tests.getCursor()
     if (pos > 0) {
       const startColumns = getAllCoords(start % 20).map(([y, x]) => x)
       const resultCoords = getAllCoords(pos % 20)
@@ -168,9 +169,9 @@ describe("builtin commands", () => {
     ist(pos <= Math.max(0, start - 200))
   }))
 
-  it("page down", !firefox && forAllValidPositions(async function (cm, start) {
+  it("page down", firefox ? undefined : forAllValidPositions(async function (cm, start) {
     await cm.sendKeys(Key.PAGE_DOWN)
-    const pos = (await tests.getSelection()).primary.anchor
+    const pos = await tests.getCursor()
     if (pos < 20*100) {
       const startColumns = getAllCoords(start % 20).map(([y, x]) => x)
       const resultCoords = getAllCoords(pos % 20)
@@ -187,7 +188,7 @@ describe("builtin commands", () => {
     if (firefox && start === 2000) return // https://github.com/codemirror/codemirror.next/issues/32
     const text = await tests.getText()
     await cm.sendKeys(Key.DELETE)
-    const pos = (await tests.getSelection()).primary.anchor
+    const pos = await tests.getCursor()
     ist(start, pos)
     const i = start % 20
     const expected = text.substr(0, start) + text.substr(start + (i >= 4 && i != 10 && i < 13 ? 2 : 1))
@@ -199,7 +200,7 @@ describe("builtin commands", () => {
   it("backspace", forAllValidPositions(async function (cm, start) {
     const text = await tests.getText()
     await cm.sendKeys(Key.BACK_SPACE)
-    const pos = (await tests.getSelection()).primary.anchor
+    const pos = await tests.getCursor()
     ist(Math.max(0, start - 1), pos)
     ist(text.substr(0, start - 1) + text.substr(start), await tests.getText())
     await tests.setText(text)
