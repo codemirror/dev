f2c979df7e4ff1f0a73a0f8c5be2a42077c955ce
Adrian Heine <mail@adrianheine.de>
2018-09-17 17:23:03 +0200

    Update rollup dependencies and include node-resolve in demo rollup config

--- rollup.demo.config.js
+++ rollup.demo.config.js
@@ -1,5 +1,6 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
+import typescript from "rollup-plugin-typescript2"
+import commonjs from "rollup-plugin-commonjs"
+import nodeResolve from "rollup-plugin-node-resolve"
 
 export default {
   input: "./demo/demo.ts",
@@ -9,13 +10,14 @@ export default {
     sourcemap: true
   },
   plugins: [
-    commonjs(),
+    nodeResolve(),
     typescript({
       check: false,
       tsconfigOverride: {
         compilerOptions: {lib: ["es5", "es6", "dom"], sourceMap: true, target: "es5", strict: false},
         include: null
       }
-    })
+    }),
+    commonjs()
   ]
 }
--- rollup.view-tests.config.js
+++ rollup.view-tests.config.js
@@ -1,4 +1,4 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from "rollup-plugin-typescript2"
 import commonjs from "rollup-plugin-commonjs"
 import nodeResolve from "rollup-plugin-node-resolve"
 
