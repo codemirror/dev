cbbda34461b5f176cb97720bf849befbbfde9d06
Marijn Haverbeke <marijn@haverbeke.nl>
2018-09-24 14:23:08 +0200

    Enable specialChars in demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -7,6 +7,7 @@ import {baseKeymap} from "../commands/src/commands"
 import {legacyMode} from "../legacy-modes/src/index"
 import {matchBrackets} from "../matchbrackets/src/matchbrackets"
 import javascript from "../legacy-modes/src/javascript"
+import {specialChars} from "../special-chars/src/special-chars"
 
 let mode = legacyMode(javascript({indentUnit: 2}, {}))
 
@@ -37,17 +38,25 @@ let isMac = /Mac/.test(navigator.platform)
 let state = EditorState.create({doc: `"use strict";
 const {readFile} = require("fs");
 
-readFile("package.json", "utf8", (err, data) => {
+readFile("pac\0\0kage.json", "utf8", (err, data) => {
   console.log(data);
-});`, plugins: [matchBrackets(), gutter(), history(), mode, keymap(baseKeymap), keymap({
-  "Mod-z": undo,
-  "Mod-Shift-z": redo,
-  "Mod-u": view => undoSelection(view) || true,
-  [isMac ? "Mod-Shift-u" : "Alt-u"]: redoSelection,
-  "Ctrl-y": isMac ? undefined : redo,
-  "Enter": crudeInsertNewlineAndIndent,
-  "Shift-Tab": crudeIndentLine
-})]})
+});`, plugins: [
+  matchBrackets(),
+  gutter(),
+  history(),
+  specialChars({}),
+  mode,
+  keymap({
+    "Mod-z": undo,
+    "Mod-Shift-z": redo,
+    "Mod-u": view => undoSelection(view) || true,
+    [isMac ? "Mod-Shift-u" : "Alt-u"]: redoSelection,
+    "Ctrl-y": isMac ? undefined : redo,
+    "Enter": crudeInsertNewlineAndIndent,
+    "Shift-Tab": crudeIndentLine
+  }),
+  keymap(baseKeymap),
+]})
 
 let view = (window as any).view = new EditorView(state)
 document.querySelector("#editor").appendChild(view.dom)
