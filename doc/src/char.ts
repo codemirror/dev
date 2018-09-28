c0539d9462fae4cd0465b40d835c79a0db056e1f
Marijn Haverbeke <marijn@haverbeke.nl>
2018-09-28 12:30:26 +0200

    Use unicode property regexp in isWordChar when supported

--- doc/src/char.ts
+++ doc/src/char.ts
@@ -8,7 +8,13 @@ export function isExtendingChar(ch: string): boolean {
 
 const nonASCIISingleCaseWordChar = /[\u00df\u0587\u0590-\u05f4\u0600-\u06ff\u3040-\u309f\u30a0-\u30ff\u3400-\u4db5\u4e00-\u9fcc\uac00-\ud7af]/
 
+let wordChar: RegExp | null
+try { wordChar = new RegExp("[\\p{Alphabetic}_]", "u") } catch (_) {}
+
+// FIXME this doesn't work for astral chars yet (need different calling convention)
+
 function isWordCharBasic(ch: string): boolean {
+  if (wordChar) return wordChar.test(ch)
   return /\w/.test(ch) || ch > "\x80" &&
     (ch.toUpperCase() != ch.toLowerCase() || nonASCIISingleCaseWordChar.test(ch))
 }
