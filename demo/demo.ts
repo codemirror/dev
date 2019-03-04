e6bb4cf4b5cd5fa5d9baec9daefb6612ec4e29a4
Marijn Haverbeke <marijn@haverbeke.nl>
2019-03-04 21:01:31 +0100

    Start overhauling gutters to be more generic

--- demo/demo.ts
+++ demo/demo.ts
@@ -2,7 +2,7 @@ import {EditorState, EditorSelection} from "../state/src"
 import {EditorView} from "../view/src/"
 import {keymap} from "../keymap/src/keymap"
 import {history, redo, redoSelection, undo, undoSelection} from "../history/src/history"
-import {gutter} from "../gutter/src/index"
+import {gutter, GutterMarkerType} from "../gutter/src/index"
 import {baseKeymap, indentSelection} from "../commands/src/commands"
 import {legacyMode} from "../legacy-modes/src/index"
 import {matchBrackets} from "../matchbrackets/src/matchbrackets"
@@ -12,6 +12,10 @@ import {multipleSelections} from "../multiple-selections/src/multiple-selections
 
 let mode = legacyMode({mode: javascript({indentUnit: 2}, {}) as any})
 
+let testMarker = new class extends GutterMarkerType<string> {
+  toDOM(markers: string[]) { return document.createTextNode(markers.join("/")) }
+}
+
 let isMac = /Mac/.test(navigator.platform)
 let state = EditorState.create({doc: `"use strict";
 const {readFile} = require("fs");
@@ -19,7 +23,7 @@ const {readFile} = require("fs");
 readFile("package.json", "utf8", (err, data) => {
   console.log(data);
 });`, extensions: [
-  gutter(),
+  gutter({class: "my-gutter", marker: testMarker}),
   history(),
   specialChars(),
   multipleSelections(),
@@ -38,3 +42,8 @@ readFile("package.json", "utf8", (err, data) => {
 
 let view = (window as any).view = new EditorView({state})
 document.querySelector("#editor").appendChild(view.dom)
+
+window.add = () => {
+  let line = view.state.doc.lineAt(view.state.selection.primary.from)
+  view.dispatch(view.state.t().addMeta(testMarker.update({markers: [testMarker.make(line.start, "X")]})))
+}
--- gutter/src/index.ts
+++ gutter/src/index.ts
@@ -1,5 +1,6 @@
-import {combineConfig, Full} from "../../extension/src/extension"
-import {EditorView, ViewExtension, ViewPlugin, styleModule, viewPlugin, BlockType, BlockInfo} from "../../view/src"
+import {fillConfig, Full, Slot, SlotType} from "../../extension/src/extension"
+import {EditorView, ViewExtension, ViewPlugin, ViewUpdate, styleModule, viewPlugin, BlockType, BlockInfo} from "../../view/src"
+import {ChangeSet, MapMode} from "../../state/src"
 import {StyleModule} from "style-mod"
 
 // FIXME Think about how the gutter width changing could cause
@@ -8,17 +9,176 @@ import {StyleModule} from "style-mod"
 // that it has to do another layout check when the gutter's width
 // changes, which should be relatively rare)
 
-// FIXME at some point, add support for custom gutter space and
-// per-line markers
+export class GutterMarker<T> {
+  constructor(readonly pos: number, readonly value: T) {}
+}
 
-// FIXME seriously slow on Firefox when devtools are open
+export abstract class GutterMarkerType<T> {
+  update: SlotType<{markers?: GutterMarker<T>[], replace?: {from: number, to: number}}>
+  constructor() { this.update = Slot.define() }
+  eq(a: T, b: T): boolean { return a === b }
+  abstract toDOM(markers: ReadonlyArray<T>): Node
+  make(pos: number, value: T) { return new GutterMarker(pos, value) }
+}
 
 export interface GutterConfig {
+  class: string,
+  marker: GutterMarkerType<any>
+  fixed?: boolean,
+  renderEmptyElements?: boolean
+}
+
+export function gutter<T>(config: GutterConfig): ViewExtension {
+  let conf = fillConfig(config, {fixed: true, renderEmptyElements: false})
+  return ViewExtension.all(
+    viewPlugin(view => new GutterView(view, conf)),
+    styleModule(styles)
+  )
+}
+
+function mapMarkers(positions: number[], changes: ChangeSet) {
+  if (changes.changes.length) for (let i = 0; i < positions.length; i++) {
+    let pos = positions[i]
+    if (pos > -1) {
+      let newPos = changes.mapPos(pos, -1, MapMode.TrackBefore)
+      if (newPos != pos) positions[i] = newPos < 0 ? -1 : newPos
+    }
+  }
+}
+
+function updateMarkers<T>(positions: number[], values: T[],
+                          update: {markers?: GutterMarker<T>[], replace?: {from: number, to: number}}) {
+  let newPositions: number[] = [], newValues: T[] = []
+  let add = update.markers || [], replace = update.replace
+  add.sort((a, b) => a.pos - b.pos)
+  let addI = 0, oldI = 0
+  for (;;) {
+    let nextAdd = addI < add.length ? add[addI].pos : 2e8
+    let nextOld = oldI < positions.length ? positions[oldI] : 2e8
+    if (nextAdd <= nextOld) {
+      if (nextAdd == 2e8) break
+      newPositions.push(nextAdd)
+      newValues.push(add[addI++].value)
+    } else {
+      if (nextOld > -1 && (!replace || replace.from > nextOld || replace.to < nextOld)) {
+        newPositions.push(nextOld)
+        newValues.push(values[oldI])
+      }
+      oldI++
+    }
+  }
+  return {positions: newPositions, values: newValues}
+}
+
+class GutterView implements ViewPlugin {
+  dom: HTMLElement
+  elements: GutterElement[] = []
+  markers: any[]
+  markerPositions: number[]
+
+  constructor(public view: EditorView, public config: Full<GutterConfig>) {
+    this.dom = document.createElement("div")
+    this.dom.className = "codemirror-gutter " + config.class + " " + styles.gutter
+    this.dom.setAttribute("aria-hidden", "true")
+    if (config.fixed) {
+      // FIXME IE11 fallback, which doesn't support position: sticky,
+      // by using position: relative + event handlers that realign the
+      // gutter (or just force fixed=false on IE11?)
+      this.dom.style.position = "sticky"
+    }
+    view.dom.insertBefore(this.dom, view.contentDOM)
+    this.markers = [] // FIXME allow to initialize somehow?
+    this.markerPositions = []
+    this.updateGutter()
+  }
+
+  update(update: ViewUpdate) {
+    for (let tr of update.transactions) {
+      mapMarkers(this.markerPositions, tr.changes)
+      let markerUpdate = tr.getMeta(this.config.marker.update)
+      if (markerUpdate)
+        ({values: this.markers, positions: this.markerPositions} = updateMarkers(this.markerPositions, this.markers, markerUpdate))
+    }
+    // FIXME would be nice to be able to recognize updates that didn't redraw
+    this.updateGutter()
+  }
+
+  updateGutter() {
+    let i = 0, height = 0
+    let markerI = 0, localMarkers: any[] = []
+    this.view.viewportLines(line => {
+      let text: BlockInfo | undefined
+      if (Array.isArray(line.type)) text = line.type.find(b => b.type == BlockType.Text)
+      else text = line.type == BlockType.Text ? line : undefined
+      if (!text) return
+
+      while (markerI < this.markers.length && this.markerPositions[markerI] <= line.from) {
+        if (this.markerPositions[markerI] == line.from) localMarkers.push(this.markers[markerI])
+        markerI++
+      }
+      if (localMarkers.length || this.config.renderEmptyElements) {
+        let above = text.top - height
+        if (i == this.elements.length) {
+          let newElt = new GutterElement(this.config, text.height, above, localMarkers)
+          this.elements.push(newElt)
+          this.dom.appendChild(newElt.dom)
+        } else {
+          this.elements[i].update(this.config, text.height, above, localMarkers)
+        }
+        height = text.bottom
+        i++
+        if (localMarkers.length) localMarkers = []
+      }
+    }, 0)
+    while (this.elements.length > i) this.dom.removeChild(this.elements.pop()!.dom)
+    this.dom.style.minHeight = this.view.contentHeight + "px"
+  }
+
+  destroy() {
+    this.dom.remove()
+  }
+
+  get styles() { return styles }
+}
+
+class GutterElement {
+  dom: HTMLElement
+  height: number = -1
+  above: number = 0
+  markers!: ReadonlyArray<any>
+
+  constructor(config: Full<GutterConfig>, height: number, above: number, markers: ReadonlyArray<any>) {
+    this.dom = document.createElement("div")
+    this.dom.className = "codemirror-gutter-element"
+    this.update(config, height, above, markers)
+  }
+
+  update(config: Full<GutterConfig>, height: number, above: number, markers: ReadonlyArray<any>) {
+    if (this.height != height)
+      this.dom.style.height = (this.height = height) + "px"
+    if (this.above != above)
+      this.dom.style.marginTop = (this.above = above) ? above + "px" : ""
+    if (!this.markers || !sameMarkers(config.marker, markers, this.markers)) {
+      this.markers = markers
+      for (let ch; ch = this.dom.lastChild;) ch.remove()
+      if (markers.length) this.dom.appendChild(config.marker.toDOM(markers))
+    }
+  }
+}
+
+function sameMarkers<T>(type: GutterMarkerType<T>, a: ReadonlyArray<T>, b: ReadonlyArray<T>): boolean {
+  if (a.length != b.length) return false
+  for (let i = 0; i < a.length; i++) if (type.eq(a[i], b[i])) return false
+  return true
+}
+
+/*
+interface LineNumberConfig {
   fixed?: boolean,
   formatNumber?: (lineNo: number) => string
 }
 
-export const gutter = ViewExtension.unique<GutterConfig>(configs => {
+export const lineNumbers = ViewExtension.unique<LineNumberConfig>(configs => {
   let config = combineConfig(configs, {
     fixed: true,
     formatNumber: String
@@ -31,7 +191,6 @@ export const gutter = ViewExtension.unique<GutterConfig>(configs => {
 
 class GutterView implements ViewPlugin {
   dom: HTMLElement
-  spaceAbove: number = 0
   lines: GutterLine[] = []
   lastLine: GutterLine
   formatNumber: (lineNo: number) => string
@@ -48,7 +207,6 @@ class GutterView implements ViewPlugin {
       this.dom.style.position = "sticky"
     }
     view.dom.insertBefore(this.dom, view.contentDOM)
-    this.formatNumber = config.formatNumber
     this.lastLine = new GutterLine(1, 0, 0, this.formatNumber)
     this.lastLine.dom.style.cssText += "visibility: hidden; pointer-events: none"
     this.dom.appendChild(this.lastLine.dom)
@@ -104,13 +262,12 @@ class GutterLine {
   above: number = 0
   below: number = 0
 
-  constructor(lineNo: number, height: number, above: number, formatNo: (lineNo: number) => string) {
+  constructor(lineNo: number, height: number, above: number) {
     this.dom = document.createElement("div")
     this.dom.className = "codemirror-gutter-element"
-    this.update(lineNo, height, above, formatNo)
   }
 
-  update(lineNo: number, height: number, above: number, formatNo: (lineNo: number) => string) {
+  update(lineNo: number, height: number, above: number, config: Full<GutterConfig>) {
     if (this.lineNo != lineNo)
       this.dom.textContent = formatNo(this.lineNo = lineNo)
     if (this.height != height)
@@ -119,22 +276,33 @@ class GutterLine {
       this.dom.style.marginTop = (this.above = above) ? above + "px" : ""
   }
 }
+*/
 
 const styles = new StyleModule({
   gutter: {
-    background: "#f5f5f5",
-    borderRight: "1px solid silver",
     display: "flex !important", // Necessary -- prevents margin collapsing
     flexDirection: "column",
+    flexShrink: 0,
+    left: 0,
+    boxSizing: "border-box",
+    height: "100%",
+    overflow: "hidden",
 
     "& > .codemirror-gutter-element": {
-      boxSizing: "border-box",
-      // FIXME these are line number specific
+      boxSizing: "border-box"
+    }
+  },
+
+  lineNumberGutter: {
+    background: "#f5f5f5",
+    borderRight: "1px solid silver",
+
+    "& > .codemirror-gutter-element": {
       padding: "0 3px 0 5px",
       minWidth: "20px",
       textAlign: "right",
       color: "#999",
       whiteSpace: "nowrap"
-    }
+    }    
   }
 })
