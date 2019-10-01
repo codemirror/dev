7a02670ab3773b5326b6f850929e302ea235e4b6
Marijn Haverbeke <marijn@haverbeke.nl>
2019-10-01 10:34:58 +0200

    Make the view tests run (under demo/test for now)

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,18 @@
 import typescript from "rollup-plugin-typescript2"
 import commonjs from "rollup-plugin-commonjs"
 
+let mode = process.env.DEVSERVER ? "devserver" : "build"
+
 let cjs = commonjs()
 let result = []
+
+function external(id) { return !/^\.?\//.test(id) }
 
 function config(module, format) {
   let [_, base, file] = /^(.+?)\/src\/(.*?)\.ts/.exec(module)
   return {
     input: `./${module}`,
-    external: id => !/^\.?\//.test(id),
+    external,
     output: {
       format,
       file: `./${base}/dist/${file}.${format == "cjs" ? "js" : "esm"}`,
@@ -30,8 +34,6 @@ function config(module, format) {
   }
 }
 
-let esm = !process.env.NO_ESM
-
 for (let module of ["text/src/index.ts",
                     "extension/src/extension.ts",
                     "state/src/index.ts",
@@ -51,25 +53,49 @@ for (let module of ["text/src/index.ts",
                     "lang-css/src/css.ts",
                     "lang-html/src/html.ts"]) {
   result.push(config(module, "cjs"))
-  if (esm) result.push(config(module, "esm"))
+  if (mode == "build") result.push(config(module, "esm"))
 }
 
 
-if (process.env.DEMO) result.push({
-  input: `./demo/demo.ts`,
-  external: id => !/^\.?\//.test(id),
-  output: {
-    format: "cjs",
-    file: "./demo/demo.js",
-    sourcemap: true,
-  },
-  plugins: [typescript({
-    tsconfigOverride: {
-      compilerOptions: {declaration: false},
-      include: [`./demo/*.ts`]
-    }
-  }), cjs]
-})
+if (mode == "devserver") {
+  result.push({
+    input: `./demo/demo.ts`,
+    external,
+    output: {
+      format: "cjs",
+      file: "./demo/demo.js",
+      sourcemap: true,
+    },
+    plugins: [typescript({
+      tsconfigOverride: {
+        compilerOptions: {declaration: false},
+        include: [`./demo/*.ts`]
+      }
+    }), cjs]
+  }, {
+    input: `./view/test/test.ts`,
+    external,
+    output: {
+      format: "cjs",
+      file: "./demo/test/test.js",
+      sourcemap: true,
+      paths: id => id == ".." ? "../../view" : null
+    },
+    plugins: [typescript({
+      tsconfigOverride: {
+        compilerOptions: {declaration: false},
+        include: [`./view/test/*.ts`]
+      }
+    }), cjs]
+  })
 
+  let root = "./demo"
+  let moduleserver = new (require("moduleserve/moduleserver"))({root})
+  let ecstatic = require("ecstatic")({root})
+  require("http").createServer((req, resp) => {
+    moduleserver.handleRequest(req, resp) || ecstatic(req, resp)
+  }).listen(8090, process.env.OPEN ? undefined : "127.0.0.1")
+  console.log("Dev server listening on 8090")
+}
 
 export default result
