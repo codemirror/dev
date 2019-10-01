f071b2eb2dcac3d7f6aa97159e02bff7b6fff886
Marijn Haverbeke <marijn@haverbeke.nl>
2019-10-01 15:10:15 +0200

    Remove browsertests
    They are severely bitrotted at this point, and can be fished from
    the history if we want to revive them.

--- browsertests/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd demo/ && python -m SimpleHTTPServer &
-httpserver=$!
-sleep 1
-
-TARGET=http://localhost:8000 npx mocha -r ts-node/register/transpile-only -t 8000 browsertests/cases/test-*.ts $@
-
-kill $httpserver
