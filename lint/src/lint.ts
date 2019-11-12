cab80c4b7bcf42f70d5fc258b8ee71900578e592
Marijn Haverbeke <marijn@haverbeke.nl>
2019-11-12 12:35:02 +0100

    Allow multiple diagnostics in a tooltip, basic styling

--- lint/src/lint.ts
+++ lint/src/lint.ts
@@ -2,12 +2,10 @@ import {EditorView, ViewPlugin, Decoration, MarkDecorationSpec, ViewUpdate} from
 import {Annotation} from "../../state"
 import {hoverTooltip, HoverTooltip} from "../../tooltip"
 
-export enum Severity { Hint, Warning, Error }
-  
 export interface Diagnostic {
   from: number
   to: number
-  severity: Severity
+  severity: "info" | "warning" | "error"
   source?: string
   message: string
   actions?: readonly Action[]
@@ -24,7 +22,8 @@ export const lint = EditorView.extend.unique<null>(() => {
   let plugin = ViewPlugin.create(view => new LintPlugin(view)).decorations(p => p.diagnostics)
   return [
     plugin.extension,
-    hoverTooltip((view, check) => view.plugin(plugin)!.hoverTooltip(view, check))
+    hoverTooltip((view, check) => view.plugin(plugin)!.hoverTooltip(view, check)),
+    EditorView.theme(defaultTheme)
   ]
 }, null)
 
@@ -48,20 +47,50 @@ class LintPlugin {
   }
 
   hoverTooltip(view: EditorView, check: (from: number, to: number) => boolean) {
-    let found: HoverTooltip | null = null
+    let found: Diagnostic[] = [], stackStart = 2e8, stackEnd = 0
     this.diagnostics.between(0, view.state.doc.length, (start, end, {spec}) => {
-      if (check(start, end)) found = {
-        pos: start, end,
-        dom: this.renderTooltip(spec.diagnostic as Diagnostic),
-        hideOnChange: true
+      if (check(start, end)) {
+        found.push(spec.diagnostic)
+        stackStart = Math.min(start, stackStart)
+        stackEnd = Math.max(end, stackEnd)
       }
     })
-    return found
+    return found.length ? {
+      pos: stackStart, end: stackEnd,
+      dom: this.renderTooltip(found),
+      style: "lint",
+      hideOnChange: true
+    } : null
   }
 
-  renderTooltip(diagnostic: Diagnostic) {
+  renderDiagnostic(diagnostic: Diagnostic) {
     let dom = document.createElement("div")
     dom.textContent = diagnostic.message
+    dom.className = this.view.cssClass("diagnostic." + diagnostic.severity)
     return dom
+  }
+
+  renderTooltip(diagnostics: Diagnostic[]) {
+    let dom = document.createElement("div")
+    for (let d of diagnostics) dom.appendChild(this.renderDiagnostic(d))
+    return dom
+  }
+}
+
+const defaultTheme = {
+  "tooltip.lint": {
+    borderLeft: "none"
+  },
+
+  diagnostic: {
+    padding: "3px 6px 3px 8px"
+  },
+
+  "diagnostic.error": {
+    borderLeft: "2px solid #b11"
+  },
+
+  "diagnostic.warning": {
+    borderLeft: "2px solid orange"
   }
 }
