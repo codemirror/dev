4150d2cd6c6673e8dd23b695d88ec85cd8947b1b
Luigi <luismastrangelo@gmail.com>
2020-04-05 02:57:12 +0200

    Improve tests of getLinesAcrossRange

--- comment/test/test-comment.ts
+++ comment/test/test-comment.ts
@@ -1,13 +1,13 @@
 import ist from "ist"
 // import {handleInsertion, handleBackspace} from "@codemirror/next/closebrackets"
-import {EditorState, EditorSelection, SelectionRange} from "@codemirror/next/state"
-// import {Text} from "@codemirror/next/text"
+import {SelectionRange} from "@codemirror/next/state"
+import {Text} from "@codemirror/next/text"
 // import {StreamSyntax} from "@codemirror/next/stream-syntax"
 import { getLinesAcrossRange } from "@codemirror/next/comment"
 
-function s(doc = "", anchor = 0, head = anchor) {
-  return EditorState.create({doc, selection: EditorSelection.single(anchor, head)})
-}
+// function s(doc = "", anchor = 0, head = anchor) {
+//   return EditorState.create({doc, selection: EditorSelection.single(anchor, head)})
+// }
 
 // function same(s0: null | {doc: Text, selection: EditorSelection}, s1: {doc: Text, selection: EditorSelection}) {
 //   ist(s0)
@@ -15,17 +15,60 @@ function s(doc = "", anchor = 0, head = anchor) {
 //   ist(JSON.stringify(s0!.selection), JSON.stringify(s1.selection))
 // }
 
-//                 0          1           2          3 
-//                 0123456 7890123 456789 0123456 7890123456789
-let testState = s("Line 1\nLine 2\nLine 3\nLine 4\Line 5")
+let lines = [
+      '<script>',
+      '  // This is a line comment',
+      '  const /* inline block-comment */ {readFile} = require("fs");',
+      '  /* This is a block comment',
+      '     spanning multiple lines */',
+      '  readFile("package.json", "utf8", (err, data) => { });',
+      '</script>',
+      '',
+      '<!-- HTML only provides',
+      '     block comments -->']
+
+let testDoc = Text.of(lines)
+
+function find(line: number, column: number): number {
+  let pos = 0;
+  for (let i = 0; i < line - 1; i++) {
+    pos += lines[i].length + 1;
+  }
+  return pos + column - 1;
+}
+
+//                 0          1          2           3           4
+//                 0123456 7890123 4567890 1234567 8901234 5 67890
+
+      // Text.of((config.doc || "").split(configuration.staticFacet(EditorState.lineSeparator) || DefaultSplit))
+// let testState = s("Line 1\nLine 2\nLine 3\nLine 4\nLine 5\n\n")
+
+// let testState = s(testDoc)
 
 describe("comment", () => {
   it("get lines across range", () => {
-    ist(getLinesAcrossRange(testState.doc, new SelectionRange(0, 6)).map(l => l.start).join(","), "0")
-    ist(getLinesAcrossRange(testState.doc, new SelectionRange(4, 8)).map(l => l.start).join(","), "0,7")
-    ist(getLinesAcrossRange(testState.doc, new SelectionRange(3, 17)).map(l => l.start).join(","), "0,7,14")
+    const t = (from: number, to: number, expectedLinesNo: number[]) => {
+      let lines = getLinesAcrossRange(testDoc, new SelectionRange(from, to))
+      ist(lines.map(l => l.start).join(","), expectedLinesNo.join(","))
+    }
+
+    t(find(1, 1), find(1, 1), [find(1, 1)])
+    t(find(2, 1), find(2, 1), [find(2, 1)])
+    t(find(7, 1), find(7, 1), [find(7, 1)])
+    // t(find(1, 1), find(lines.length, lines[lines.length-1].length + 1), [find(7, 1)])
+    // t(0, 35, [0,7,14,21,28,35])
+    // t(0, 6, [0])
+    // t(4, 8, [0,7])
+    // t(3, 17, [0,7,14])
   })
 
+  it("get range", () => {
+    // const t = (from: number, to: number, expectedLinesNo: number[]) => {
+    //   toggleComment(CommentOption.Toggle, )
+    //   let lines = getLinesAcrossRange(testState.doc, new SelectionRange(from, to))
+    //   ist(lines.map(l => l.start).join(","), expectedLinesNo.join(","))
+    // }
+})
   // const syntax = new StreamSyntax({
   //   docProps: [[languageData, {closeBrackets: {brackets: ["(", "'", "'''"]}}]],
   //   token(stream) {
