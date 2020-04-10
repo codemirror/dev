1b1675c0b08cf225a11bc04ba7ed8cc128b9dddb
Luigi <luismastrangelo@gmail.com>
2020-04-10 02:58:53 +0200

    Add early support for block comments

--- comment/src/comment.ts
+++ comment/src/comment.ts
@@ -14,11 +14,11 @@ export const toggleCommentCmd: Command = view => {
 }
 
 export const commentCmd: Command = view => {
-    return dispatchToggleComment(CommentOption.OnlyComment, view)
+  return dispatchToggleComment(CommentOption.OnlyComment, view)
 }
 
 export const uncommentCmd: Command = view => {
-    return dispatchToggleComment(CommentOption.OnlyUncomment, view)
+  return dispatchToggleComment(CommentOption.OnlyUncomment, view)
 }
 
 /// The action to be taken when toggling comments.
@@ -35,12 +35,63 @@ const dispatchToggleComment = (option: CommentOption, view: EditorView): boolean
   return true
 }
 
-export const toggleBlockComment = (option: CommentOption, blockCommentTokens: {open: string, close: string}) => (state: EditorState): Transaction | null => {
-  return null
+type LineCommentToken = string
+
+export class BlockCommenter {
+    open: string
+    close: string
+    constructor(open: string, close: string) {
+        this.open = open
+        this.close = close
+    }
+
+    toggle(option: CommentOption, state: EditorState): Transaction | null {
+      // for (const range of state.selection.ranges) {
+      //   const lines = getLinesInRange(state.doc, range)
+      //   linesAcrossSelection.push(...lines)
+      //   linesAcrossRange[k(range)] = lines
+      // }
+        // tr.replace(range.from, range.from, this.open + margin)
+        // tr.replace(range.to, range.to, margin + this.close)
+        // return tr
+      return null
+    }
+
+    /// Determines whether all selection ranges in `state`
+    /// are block-commented, i.e., if `isRangeCommented` returns
+    /// `true` for all selection ranges.
+    isSelectionCommented(state: EditorState): boolean {
+        let result = true
+        for (const range of state.selection.ranges) {
+            result = result && this.isRangeCommented(state, range)
+        }
+        return result
+    }
+
+    /// The `range` must be a valid range in `state`.
+    isRangeCommented(state: EditorState, range: SelectionRange): boolean {
+        type SearchWithType = (this: string, searchString: string, pos?: number) => boolean
+        const search = (pos: number, searchString: string, searchWith: SearchWithType, d: 1 | -1, i: 1 | 0): boolean => {
+            const line = state.doc.lineAt(pos)
+            const str = line.content as string
+          const ss = eatSpace(str, pos - line.start - i, d)
+          return searchWith.call(str, searchString, pos + (d*ss) - line.start)
+        }
+        const startsWithOpen = search(range.from, this.open, String.prototype.endsWith, -1, 1)
+        const endsWithClose = search(range.to, this.close, String.prototype.startsWith, 1, 0)
+        return startsWithOpen && endsWithClose
+    }
+
+    ///
+    insert(tr: Transaction, range: SelectionRange, margin: string = " "): Transaction {
+        tr.replace(range.from, range.from, this.open + margin)
+        tr.replace(range.to, range.to, margin + this.close)
+        return tr
+    }
 }
 
 /// TODO: Add docs
-export const toggleLineComment = (option: CommentOption, lineCommentToken: string) => (state: EditorState): Transaction | null => {
+export const toggleLineComment = (option: CommentOption, lineCommentToken: LineCommentToken) => (state: EditorState): Transaction | null => {
   const k = (range: SelectionRange): string => range.anchor + "," + range.head
   const linesAcrossSelection: Line[] = []
   const linesAcrossRange : { [id: string] : Line[]; } = {};
@@ -114,11 +165,13 @@ export const insertLineComment = (tr: Transaction, pos: number, lineCommentToken
   return tr.replace(pos, pos, lineCommentToken + margin)
 }
 
+/// Removes a line-comment at the given `pos`.
 /// See `insertLineComment`.
 export const removeLineComment = (tr: Transaction, pos: number, lineCommentToken: string, marginLen: number = 1): Transaction => {
   return tr.replace(pos, pos + lineCommentToken.length + marginLen, "")
 }
 
+/// Computes the lines spanned by `range`.
 /// This function is exported for testing purposes.
 export const getLinesInRange = (doc: Text, range: SelectionRange): Line[] => {
   let line: Line = doc.lineAt(range.from)
@@ -140,8 +193,17 @@ export const getLinesInRange = (doc: Text, range: SelectionRange): Line[] => {
 /// non-whitespace character.
 /// Note that in case of all characters are whitespace,
 /// it will return the length of `str`.
-const eatSpace = (str: string): number => {
+export const _eatSpace = (str: string): number => {
   let pos = 0
   while (/[\s\u00a0]/.test(str.charAt(pos))) ++pos
   return pos
+}
+
+const eatSpace = (str: string, pos: number = 0, direction: 1 | -1 = 1): number => {
+  let count = 0
+  while (/[\s\u00a0]/.test(str.charAt(pos))) {
+    count++
+    pos += direction
+  }
+  return count
 }
