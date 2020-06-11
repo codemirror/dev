52a9e645326145befdca2784ea7b75428517eef6
Marijn Haverbeke <marijn@haverbeke.nl>
2020-06-11 13:10:10 +0200

    Clean up one-dark theme definition

--- theme-one-dark/src/one-dark.ts
+++ theme-one-dark/src/one-dark.ts
@@ -2,18 +2,33 @@ import {EditorView} from "@codemirror/next/view"
 import {Extension} from "@codemirror/next/state"
 import {highlighter} from "@codemirror/next/highlight"
 
+const chalky = "#e5c07b",
+  coral = "#e06c75",
+  dark = "#5c6370",
+  fountainBlue = "#56b6c2",
+  green = "#98c379",
+  invalid = "#ffffff",
+  lightDark = "#7f848e",
+  lightWhite = "#abb2bf",
+  malibu = "#61afef",
+  purple = "#c678dd",
+  whiskey = "#d19a66",
+  background = "#282c34",
+  selection = "#405948",
+  cursor = "#528bff"
+
 export const oneDarkTheme = EditorView.theme({
   wrap: {
-    color: "#abb2bf",
-    backgroundColor: "#282c34",
-    "& ::selection": {backgroundColor: "#405948"},
-    caretColor: "#528bff"
+    color: lightWhite,
+    backgroundColor: background,
+    "& ::selection": {backgroundColor: selection},
+    caretColor: cursor
   },
 
-  secondaryCursor: {borderLeft: "1.4px solid #528bff"},
-  secondarySelection: {backgroundColor: "#405948"},
+  secondaryCursor: {borderLeft: `1.4px solid ${cursor}`},
+  secondarySelection: {backgroundColor: selection},
 
-  panels: {backgroundColor: "#282c34", color: "#abb2bf"},
+  panels: {backgroundColor: background, color: lightWhite},
   "panels.top": {borderBottom: "2px solid black"},
   "panels.bottom": {borderTop: "2px solid black"},
 
@@ -28,14 +43,17 @@ export const oneDarkTheme = EditorView.theme({
   activeLine: {backgroundColor: "#2c313c"},
   selectionMatch: {backgroundColor: "#354139"},
 
-  "matchingBracket, nonmatchingBracket": {backgroundColor: "#515a6b", border: "1px solid #515a6b"},
+  "matchingBracket, nonmatchingBracket": {
+    backgroundColor: "#515a6b",
+    border: "1px solid #515a6b"
+  },
 
   gutters: {
-    backgroundColor: "#282c34",
+    backgroundColor: background,
     color: "#495162",
     border: "none"
   },
-  "gutterElement.lineNumber": {color: "#495162"},
+  "gutterElement.lineNumber": {color: "inherit"},
 
   foldPlaceholder: {
     backgroundColor: "none",
@@ -48,21 +66,9 @@ export const oneDarkTheme = EditorView.theme({
     backgroundColor: "#606862"
   },
   "tooltip.autocomplete": {
-    "& > li[aria-selected]": {backgroundColor: "#282c34"}
+    "& > li[aria-selected]": {backgroundColor: background}
   }
 }, {dark: true})
-
-const chalky = "#e5c07b",
-  coral = "#e06c75",
-  dark = "#5c6370",
-  fountainBlue = "#56b6c2",
-  green = "#98c379",
-  invalid = "#ffffff",
-  lightDark = "#7f848e",
-  lightWhite = "#abb2bf",
-  malibu = "#61afef",
-  purple = "#c678dd",
-  whiskey = "#d19a66"
 
 export const oneDarkHighlighter = highlighter({
   invalid: {color: invalid},
