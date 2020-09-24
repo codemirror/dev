79430ea3fd6c1c4b7d9685fa497aac0b60ca9f09
Philipp Raab <philipp.raab@dynatrace.com>
2020-09-24 08:14:48 +0200

    Add json language package

---
+++ lang-json/src/json.ts
@@ -0,0 +1,36 @@
+import {parser} from "lezer-json"
+import {continuedIndent, indentNodeProp, foldNodeProp, LezerSyntax} from "@codemirror/next/syntax"
+import {Extension} from "@codemirror/next/state"
+import {Subtree} from "lezer-tree"
+import {styleTags} from "@codemirror/next/highlight"
+
+export const jsonSyntax = LezerSyntax.define(parser.withProps(
+  indentNodeProp.add({
+    Object: continuedIndent({except: /^\s*\}/}),
+    Array: continuedIndent({except: /^\s*\]/})
+  }),
+  foldNodeProp.add({
+    Object(subtree: Subtree) { return {from: subtree.start + 1, to: subtree.end - 1} },
+    Array(subtree: Subtree) { return {from: subtree.start + 1, to: subtree.end - 1} }
+  }),
+  styleTags({
+    String: "string",
+    Number: "number",
+    "True False": "bool",
+    PropertyName: "propertyName",
+    null: "null",
+    ",": "separator",
+    "[ ]": "squareBracket",
+    "{ }": "brace"
+  })
+), {
+  languageData: {
+    closeBrackets: {brackets: ["[", "{", '"']},
+    indentOnInput: /^\s*[\}\]]$/
+  }
+})
+
+/// Returns an extension that installs the JSON syntax provider.
+export function json(): Extension {
+  return jsonSyntax
+}
