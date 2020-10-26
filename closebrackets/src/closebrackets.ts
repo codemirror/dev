a0ddc17c7cca743dc89fe3f9a12158ee0d0da2e5
Marijn Haverbeke <marijn@haverbeke.nl>
2020-10-26 12:27:29 +0100

    Make closeBrackets only skip over brackets it itself added
    See https://github.com/codemirror/CodeMirror/issues/6430
    
    FIX: `closeBrackets` will no longer type-over brackets that it did
    not itself insert.

--- closebrackets/src/closebrackets.ts
+++ closebrackets/src/closebrackets.ts
@@ -1,5 +1,7 @@
 import {EditorView, KeyBinding} from "@codemirror/next/view"
-import {EditorState, EditorSelection, Transaction, CharCategory, Extension, StateCommand} from "@codemirror/next/state"
+import {EditorState, EditorSelection, Transaction, CharCategory, Extension,
+        StateCommand, StateField, StateEffect, MapMode} from "@codemirror/next/state"
+import {RangeSet, RangeValue} from "@codemirror/next/rangeset"
 import {Text} from "@codemirror/next/text"
 import {codePointAt, fromCodePoint, codePointSize} from "@codemirror/next/text"
 
@@ -22,12 +24,43 @@ const defaults: Required<CloseBracketConfig> = {
   before: ")]}'\":;>"
 }
 
+const closeBracketEffect = StateEffect.define<number>({
+  map(value, mapping) {
+    let mapped = mapping.mapPos(value, -1, MapMode.TrackAfter)
+    return mapped == null ? undefined : mapped
+  }
+})
+const skipBracketEffect = StateEffect.define<number>({
+  map(value, mapping) { return mapping.mapPos(value) }
+})
+
+const closedBracket = new class extends RangeValue {}
+closedBracket.startSide = 1; closedBracket.endSide = -1
+
+const bracketState = StateField.define<RangeSet<typeof closedBracket>>({
+  create() { return RangeSet.empty },
+  update(value, tr) {
+    if (tr.selection) {
+      let lineStart = tr.state.doc.lineAt(tr.selection.primary.head).from
+      let prevLineStart = tr.startState.doc.lineAt(tr.startState.selection.primary.head).from
+      if (lineStart != tr.changes.mapPos(prevLineStart, -1))
+        value = RangeSet.empty
+    }
+    value = value.map(tr.changes)
+    for (let effect of tr.effects) {
+      if (effect.is(closeBracketEffect)) value = value.update({add: [closedBracket.range(effect.value, effect.value + 1)]})
+      else if (effect.is(skipBracketEffect)) value = value.update({filter: from => from != effect.value})
+    }
+    return value
+  }
+})
+
 /// Extension to enable bracket-closing behavior. When a closeable
 /// bracket is typed, its closing bracket is immediately inserted
 /// after the cursor. When closing a bracket directly in front of that
 /// closing bracket, the cursor moves over the existing bracket.
 export function closeBrackets(): Extension {
-  return EditorView.inputHandler.of(handleInput)
+  return [EditorView.inputHandler.of(handleInput), bracketState]
 }
 
 const definedClosing = "()[]{}<>"
@@ -89,10 +122,18 @@ export function handleInsertion(state: EditorState, ch: string): Transaction | n
     if (ch == tok)
       return closed == tok ? handleSame(state, tok, tokens.indexOf(tok + tok + tok) > -1) 
         : handleOpen(state, tok, closed, conf.before || defaults.before)
-    if (ch == closed)
+    if (ch == closed && closedBracketAt(state, state.selection.primary.from))
       return handleClose(state, tok, closed)
   }
   return null
+}
+
+function closedBracketAt(state: EditorState, pos: number) {
+  let found = false
+  state.field(bracketState).between(0, state.doc.length, from => {
+    if (from == pos) found = true
+  })
+  return found
 }
 
 function nextChar(doc: Text, pos: number) {
@@ -109,14 +150,19 @@ function handleOpen(state: EditorState, open: string, close: string, closeBefore
   let dont = null, changes = state.changeByRange(range => {
     if (!range.empty)
       return {changes: [{insert: open, from: range.from}, {insert: close, from: range.to}],
+              effects: closeBracketEffect.of(range.to + open.length),
               range: EditorSelection.range(range.anchor + open.length, range.head + open.length)}
     let next = nextChar(state.doc, range.head)
     if (!next || /\s/.test(next) || closeBefore.indexOf(next) > -1)
       return {changes: {insert: open + close, from: range.head},
+              effects: closeBracketEffect.of(range.head + open.length),
               range: EditorSelection.cursor(range.head + open.length)}
     return {range: dont = range}
   })
-  return dont ? null : state.update(changes, {scrollIntoView: true, annotations: Transaction.userEvent.of("input")})
+  return dont ? null : state.update(changes, {
+    scrollIntoView: true,
+    annotations: Transaction.userEvent.of("input")
+  })
 }
 
 function handleClose(state: EditorState, _open: string, close: string) {
@@ -124,8 +170,11 @@ function handleClose(state: EditorState, _open: string, close: string) {
     if (range.empty && nextChar(state.doc, range.head) == close) return EditorSelection.cursor(range.head + close.length)
     return dont = range
   })
-  return dont ? null : state.update({selection: EditorSelection.create(moved, state.selection.primaryIndex),
-                                 scrollIntoView: true})
+  return dont ? null : state.update({
+    selection: EditorSelection.create(moved, state.selection.primaryIndex),
+    scrollIntoView: true,
+    effects: state.selection.ranges.map(({from}) => skipBracketEffect.of(from))
+  })
 }
 
 // Handles cases where the open and close token are the same, and
@@ -134,29 +183,37 @@ function handleSame(state: EditorState, token: string, allowTriple: boolean) {
   let dont = null, changes = state.changeByRange(range => {
     if (!range.empty)
       return {changes: [{insert: token, from: range.from}, {insert: token, from: range.to}],
+              effects: closeBracketEffect.of(range.to + token.length),
               range: EditorSelection.range(range.anchor + token.length, range.head + token.length)}
     let pos = range.head, next = nextChar(state.doc, pos)
     if (next == token) {
       if (nodeStart(state, pos)) {
         return {changes: {insert: token + token, from: pos},
+                effects: closeBracketEffect.of(pos + token.length),
                 range: EditorSelection.cursor(pos + token.length)}
-      } else {
+      } else if (closedBracketAt(state, pos)) {
         let isTriple = allowTriple && state.sliceDoc(pos, pos + token.length * 3) == token + token + token
-        return {range: EditorSelection.cursor(pos + token.length * (isTriple ? 3 : 1))}
+        return {range: EditorSelection.cursor(pos + token.length * (isTriple ? 3 : 1)),
+                effects: skipBracketEffect.of(pos)}
       }
     } else if (allowTriple && state.sliceDoc(pos - 2 * token.length, pos) == token + token &&
                nodeStart(state, pos - 2 * token.length)) {
       return {changes: {insert: token + token + token + token, from: pos},
+              effects: closeBracketEffect.of(pos + token.length),
               range: EditorSelection.cursor(pos + token.length)}
     } else if (state.charCategorizer(pos)(next) != CharCategory.Word) {
       let prev = state.sliceDoc(pos - 1, pos)
       if (prev != token && state.charCategorizer(pos)(prev) != CharCategory.Word)
         return {changes: {insert: token + token, from: pos},
+                effects: closeBracketEffect.of(pos + token.length),
                 range: EditorSelection.cursor(pos + token.length)}
     }
     return {range: dont = range}
   })
-  return dont ? null : state.update(changes, {scrollIntoView: true, annotations: Transaction.userEvent.of("input")})
+  return dont ? null : state.update(changes, {
+    scrollIntoView: true,
+    annotations: Transaction.userEvent.of("input")
+  })
 }
 
 function nodeStart(state: EditorState, pos: number) {
--- closebrackets/test/test-closebrackets.ts
+++ closebrackets/test/test-closebrackets.ts
@@ -1,15 +1,13 @@
 import ist from "ist"
-import {handleInsertion, deleteBracketPair} from "@codemirror/next/closebrackets"
-import {EditorState, Transaction, EditorSelection, StateCommand} from "@codemirror/next/state"
+import {handleInsertion, deleteBracketPair, closeBrackets} from "@codemirror/next/closebrackets"
+import {EditorState, EditorSelection, StateCommand} from "@codemirror/next/state"
 import {StreamSyntax} from "@codemirror/next/stream-syntax"
 
 function s(doc = "", anchor = 0, head = anchor) {
-  return EditorState.create({doc, selection: EditorSelection.single(anchor, head)})
+  return EditorState.create({doc, selection: EditorSelection.single(anchor, head), extensions: closeBrackets()})
 }
 
-function same(s0: null | Transaction | EditorState, s1: EditorState) {
-  ist(s0)
-  let s: EditorState = s0 instanceof Transaction ? s0.state : s0!
+function same(s: EditorState, s1: EditorState) {
   ist(s.doc.toString(), s1.doc.toString())
   ist(JSON.stringify(s.selection), JSON.stringify(s1.selection))
 }
@@ -23,16 +21,25 @@ function canApp(s: EditorState, cmd: StateCommand) {
   return !!cmd({state: s, dispatch: () => {}})
 }
 
+function ins(s: EditorState, value: string) {
+  let result = handleInsertion(s, value)
+  return result ? result.state : s
+}
+
+function type(s: EditorState, text: string) {
+  return s.update(s.replaceSelection(text)).state
+}
+
 describe("closeBrackets", () => {
   it("closes brackets", () => {
-    same(handleInsertion(s(), "("), s("()", 1))
-    same(handleInsertion(s("foo", 3), "["), s("foo[]", 4))
-    same(handleInsertion(s(), "{"), s("{}", 1))
+    same(ins(s(), "("), s("()", 1))
+    same(ins(s("foo", 3), "["), s("foo[]", 4))
+    same(ins(s(), "{"), s("{}", 1))
   })
 
   it("closes brackets before whitespace", () => {
-    same(handleInsertion(s("foo bar", 3), "("), s("foo() bar", 4))
-    same(handleInsertion(s("\t"), "{"), s("{}\t", 1))
+    same(ins(s("foo bar", 3), "("), s("foo() bar", 4))
+    same(ins(s("\t"), "{"), s("{}\t", 1))
   })
 
   it("doesn't close brackets before regular chars", () => {
@@ -42,22 +49,22 @@ describe("closeBrackets", () => {
   })
 
   it("closes brackets before allowed chars", () => {
-    same(handleInsertion(s("foo }", 4), "("), s("foo ()}", 5))
-    same(handleInsertion(s("foo :", 4), "["), s("foo []:", 5))
+    same(ins(s("foo }", 4), "("), s("foo ()}", 5))
+    same(ins(s("foo :", 4), "["), s("foo []:", 5))
   })
 
   it("surrounds selected content", () => {
-    same(handleInsertion(s("onetwothree", 3, 6), "("), s("one(two)three", 4, 7))
-    same(handleInsertion(s("okay", 4, 0), "["), s("[okay]", 5, 1))
+    same(ins(s("onetwothree", 3, 6), "("), s("one(two)three", 4, 7))
+    same(ins(s("okay", 4, 0), "["), s("[okay]", 5, 1))
   })
 
   it("skips matching close brackets", () => {
-    same(handleInsertion(s("foo()", 4), ")"), s("foo()", 5))
-    same(handleInsertion(s("[]x", 1), "]"), s("[]x", 2))
+    same(ins(ins(s("foo", 3), "("), ")"), s("foo()", 5))
+    same(ins(ins(s("", 0), "["), "]"), s("[]", 2))
   })
 
   it("doesn't skip when there's a selection", () => {
-    ist(!handleInsertion(s("(a)", 1, 2), ")"))
+    same(ins(ins(s("a", 0, 1), "("), ")"), s("(a)", 1, 2))
   })
 
   it("doesn't skip when the next char doesn't match", () => {
@@ -65,13 +72,13 @@ describe("closeBrackets", () => {
   })
 
   it("closes quotes", () => {
-    same(handleInsertion(s(), "'"), s("''", 1))
-    same(handleInsertion(s("foo ", 4), "\""), s("foo \"\"", 5))
+    same(ins(s(), "'"), s("''", 1))
+    same(ins(s("foo ", 4), "\""), s("foo \"\"", 5))
   })
 
   it("wraps quotes around the selection", () => {
-    same(handleInsertion(s("a b c", 2, 3), "'"), s("a 'b' c", 3, 4))
-    same(handleInsertion(s("boop", 3, 1), "'"), s("b'oo'p", 4, 2))
+    same(ins(s("a b c", 2, 3), "'"), s("a 'b' c", 3, 4))
+    same(ins(s("boop", 3, 1), "'"), s("b'oo'p", 4, 2))
   })
 
   it("doesn't close quotes in words", () => {
@@ -97,23 +104,23 @@ describe("closeBrackets", () => {
   const data = syntax.languageData.of({closeBrackets: {brackets: ["(", "'", "'''"]}})
 
   function st(doc = "", anchor = 0, head = anchor) {
-    return EditorState.create({doc, selection: EditorSelection.single(anchor, head), extensions: [syntax, data]})
+    return EditorState.create({doc, selection: EditorSelection.single(anchor, head), extensions: [syntax, data, closeBrackets()]})
   }
 
   it("skips closing quotes", () => {
-    same(handleInsertion(st("'foo'", 4), "'"), st("'foo'", 5))
+    same(ins(type(ins(s(), "'"), "foo"), "'"), st("'foo'", 5))
   })
 
   it("closes triple-quotes", () => {
-    same(handleInsertion(st("''", 2), "'"), st("''''''", 3))
+    same(ins(st("''", 2), "'"), st("''''''", 3))
   })
 
   it("skips closing triple-quotes", () => {
-    same(handleInsertion(st("''''''", 3), "'"), st("''''''", 6))
+    same(ins(ins(st("''", 2), "'"), "'"), st("''''''", 6))
   })
 
   it("closes quotes before another string", () => {
-    same(handleInsertion(st("foo ''", 4), "'"), st("foo ''''", 5))
+    same(ins(st("foo ''", 4), "'"), st("foo ''''", 5))
   })
 
   it("backspaces out pairs of brackets", () => {
@@ -126,5 +133,26 @@ describe("closeBrackets", () => {
     ist(!canApp(st("(", 1), deleteBracketPair))
     ist(!canApp(st("-]", 1), deleteBracketPair))
     ist(!canApp(st("", 0), deleteBracketPair))
+  })
+
+  it("doesn't skip brackets not inserted by the addon", () => {
+    same(ins(s("()", 1), ")"), s("()", 1))
+  })
+
+  it("can remember multiple brackets", () => {
+    same(ins(ins(type(ins(type(ins(s(), "("), "foo"), "["), "x"), "]"), ")"), s("(foo[x])", 8))
+  })
+
+  it("clears state when moving to a different line", () => {
+    let state = ins(s("one\ntwo", 7), "(")
+    state = state.update({selection: {anchor: 0}}).state
+    state = state.update({selection: {anchor: 8}}).state
+    ist(!handleInsertion(state, ")"))
+  })
+
+  it("doesn't clear state for changes on different lines", () => {
+    let state = ins(s("one\ntwo", 7), "(")
+    state = state.update({changes: {insert: "x", from: 0}}).state
+    same(ins(state, ")"), s("xone\ntwo()", 10))
   })
 })
