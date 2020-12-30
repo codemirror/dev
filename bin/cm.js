fcddb21c7549592431e9079dde6655b0ed97a0ba
Marijn Haverbeke <marijn@haverbeke.nl>
2020-12-30 14:53:34 +0100

    Move package list into separate script

--- bin/cm.js
+++ bin/cm.js
@@ -6,68 +6,7 @@ const child = require("child_process"), fs = require("fs"), fsp = fs.promises, p
 
 let root = join(__dirname, "..")
 
-const core = [
-  "state",
-  "text",
-  "view",
-  "commands",
-  "history",
-  "collab",
-  "gutter",
-  "rangeset",
-  "language",
-  "language-data",
-  "fold",
-  "matchbrackets",
-  "closebrackets",
-  "panel",
-  "tooltip",
-  "search",
-  "lint",
-  "highlight",
-  "stream-parser",
-  "autocomplete",
-  "comment",
-  "rectangular-selection",
-  "basic-setup"
-]
-let nonCore = [
-  "lang-javascript",
-  "lang-java",
-  "lang-json",
-  "lang-cpp",
-  "lang-python",
-  "lang-css",
-  "lang-html",
-  "lang-sql",
-  "lang-rust",
-  "lang-xml",
-  "lang-markdown",
-  "legacy-modes",
-  "theme-one-dark"
-]
-
-class Pkg {
-  constructor(name) {
-    this.name = name
-    this.dir = join(root, name)
-    this.main = null
-    if (name != "legacy-modes" && fs.existsSync(this.dir)) {
-      let files = fs.readdirSync(join(this.dir, "src")).filter(f => /^[^.]+\.ts$/.test(f))
-      let main = files.length == 1 ? files[0] : files.includes("index.ts") ? "index.ts"
-          : files.includes(name.replace(/^(theme-|lang-)/, "") + ".ts") ? name.replace(/^(theme-|lang-)/, "") + ".ts" : null
-      if (!main) throw new Error("Couldn't find a main script for " + name)
-      this.main = join(this.dir, "src", main)
-    }
-  }
-}
-
-function loadPackages() {
-  let packages = core.concat(nonCore).map(n => new Pkg(n))
-  let packageNames = Object.create(null)
-  for (let p of packages) packageNames[p.name] = p
-  return {packages, packageNames, buildPackages: packages.filter(p => p.main)}
-}
+const {loadPackages} = require("./packages")
 
 let {packages, packageNames, buildPackages} = loadPackages()
 
---
+++ bin/packages.js
@@ -0,0 +1,66 @@
+const fs = require("fs"), {join} = require("path")
+
+exports.core = [
+  "state",
+  "text",
+  "view",
+  "commands",
+  "history",
+  "collab",
+  "gutter",
+  "rangeset",
+  "language",
+  "language-data",
+  "fold",
+  "matchbrackets",
+  "closebrackets",
+  "panel",
+  "tooltip",
+  "search",
+  "lint",
+  "highlight",
+  "stream-parser",
+  "autocomplete",
+  "comment",
+  "rectangular-selection",
+  "basic-setup"
+]
+exports.nonCore = [
+  "lang-javascript",
+  "lang-java",
+  "lang-json",
+  "lang-cpp",
+  "lang-python",
+  "lang-css",
+  "lang-html",
+  "lang-sql",
+  "lang-rust",
+  "lang-xml",
+  "lang-markdown",
+  "legacy-modes",
+  "theme-one-dark"
+]
+
+exports.all = exports.core.concat(exports.nonCore)
+
+class Pkg {
+  constructor(name) {
+    this.name = name
+    this.dir = join(__dirname, "..", name)
+    this.main = null
+    if (name != "legacy-modes" && fs.existsSync(this.dir)) {
+      let files = fs.readdirSync(join(this.dir, "src")).filter(f => /^[^.]+\.ts$/.test(f))
+      let main = files.length == 1 ? files[0] : files.includes("index.ts") ? "index.ts"
+          : files.includes(name.replace(/^(theme-|lang-)/, "") + ".ts") ? name.replace(/^(theme-|lang-)/, "") + ".ts" : null
+      if (!main) throw new Error("Couldn't find a main script for " + name)
+      this.main = join(this.dir, "src", main)
+    }
+  }
+}
+
+exports.loadPackages = function loadPackages() {
+  let packages = exports.all.map(n => new Pkg(n))
+  let packageNames = Object.create(null)
+  for (let p of packages) packageNames[p.name] = p
+  return {packages, packageNames, buildPackages: packages.filter(p => p.main)}
+}
